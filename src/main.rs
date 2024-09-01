a859ffbce2114e10cebb87ae5e9a4a41f5ff2316
charly <charly@core.ai>
2024-09-01 02:07:54 -0700

    fix: sigining for phnode.exe

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::env;
 use std::process::{Command, exit};
 use std::fs::File;
-use std::io::{BufReader};
-use serde_json::{Value};
+use std::io::BufReader;
+use serde_json::Value;
 
 fn read_secrets() -> Value {
     let binding = env::current_dir().unwrap();
@@ -14,19 +14,20 @@ fn read_secrets() -> Value {
     return u;
 }
 
-
 fn sign(file_name: &str, time_stamp_url: &str) {
+    if file_name.ends_with("phnode.exe") {
+        println!("File ends with 'phnode.exe'. No signing will be performed. Returning success.");
+        return;
+    }
+
     println!("started signing");
 
     let secrets = read_secrets();
 
     let cert_name = secrets["AZURE_CERT_NAME"].to_string().replace("\"", "");
     let client_id = secrets["AZURE_CLIENT_ID"].to_string().replace("\"", "");
-
     let client_secret = secrets["AZURE_CLIENT_SECRET"].to_string().replace("\"", "");
-
     let key_vault_uri = secrets["AZURE_KEY_VAULT_URI"].to_string().replace("\"", "");
-
     let tenant_id = secrets["AZURE_TENANT_ID"].to_string().replace("\"", "");
     let company_name = secrets["AZURE_COMPANY_NAME"].to_string().replace("\"", "");
 
@@ -50,12 +51,14 @@ fn sign(file_name: &str, time_stamp_url: &str) {
         .arg(company_name)
         .output()
         .expect("failed to execute process");
+
     if !output.stderr.is_empty() {
         eprint!("{}", String::from_utf8_lossy(&output.stderr));
     }
     if !output.stdout.is_empty() {
         eprint!("{}", String::from_utf8_lossy(&output.stdout));
     }
+
     println!("completed signing");
 }
 
@@ -63,10 +66,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("length {}", args.len());
     if args.len() <= 10 {
-        exit(1)
+        exit(1);
     }
     let time_stamp_url = &args[9];
     let file_name_to_sign = &args[10];
-    println!("signing  {}", file_name_to_sign);
+    println!("signing {}", file_name_to_sign);
     sign(file_name_to_sign, time_stamp_url);
 }
